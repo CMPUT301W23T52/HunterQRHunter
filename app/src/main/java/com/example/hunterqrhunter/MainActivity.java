b5971e0161ec9ca18350e6b7e5aaba66f2d6dc8a
Lingfeng Zhu <lingfen1@ualberta.ca>
2023-03-03 18:26:51 -0700

    Revert "edit"
    This reverts commit ada3c3f0001d35f330ef242e7f149dde8b68d2a0.

--- app/src/main/java/com/example/hunterqrhunter/MainActivity.java
+++ app/src/main/java/com/example/hunterqrhunter/MainActivity.java
@@ -17,10 +17,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
-    int a = 1;
     Object hashVal = "yongbin@gmail.com";
     HashQR hashQR = new HashQR();
     @Override
