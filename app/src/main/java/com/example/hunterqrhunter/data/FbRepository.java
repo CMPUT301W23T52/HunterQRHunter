b91666989402e679b85d246058eed560e32ef5e5
Lingfeng Zhu <lingfen1@ualberta.ca>
2023-03-08 10:54:51 -0700

    edit

--- app/src/main/java/com/example/hunterqrhunter/data/FbRepository.java
+++ app/src/main/java/com/example/hunterqrhunter/data/FbRepository.java
@@ -9,5 +9,36 @@ package com.example.hunterqrhunter.data;
 // QR code need to be a separate model in firebase database
 // QR code getter setter need to be import for firebase as well
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
+
+import com.example.hunterqrhunter.model.User;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
 public class FbRepository {
+    final private FirebaseFirestore db;
+
+    public FbRepository(FirebaseFirestore db) {
+        this.db = db;
+    }
+
+    public CollectionReference getCollectionRef(String colName) {
+        return db.collection(colName);
+    }
+
+    public DocumentReference getDocumentRef(String colName, String docName) {
+
+        return db.collection(colName).document(docName);
+    }
+
+    public void createUser(User user) {
+        Map<String, Object> userValues = user.toMap();
+        db.collection("users").add(user).addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
+                .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
+    }
 }
--- app/src/main/java/com/example/hunterqrhunter/model/User.java
+++ app/src/main/java/com/example/hunterqrhunter/model/User.java
@@ -1,12 +1,41 @@
 package com.example.hunterqrhunter.model;
 
+import com.google.firebase.firestore.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class User {
+    String uid;
     String firstName;
     String lastName;
-    String email;
+    int born;
+    int hash;
+
+    public User(String uid,String fn, String ln, int born, int hash) {
+        this.uid = uid;
+        this.firstName =  fn;
+        this.lastName = ln;
+        this.born = born;
+        this.hash = hash;
+    }
+
+
 //    Array<HashQR> qrArray;
 
     public String getFirstName() {
-        return firstName;
+        return this.firstName;
     }
-}
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", this.uid);
+        result.put("firstName", this.firstName);
+        result.put("lastName", this.lastName);
+        result.put("born", born);
+        result.put("hash", hash);
+
+        return result;
+    }}
