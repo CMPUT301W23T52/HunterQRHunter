415e2483785c90e313a4d86b8440392c57d91a0c
dohyunkim <kdhminime@gmail.com>
2023-03-09 14:12:00 -0700

    user screen added

--- app/src/main/java/com/example/hunterqrhunter/LoginScreen.java
+++ app/src/main/java/com/example/hunterqrhunter/LoginScreen.java
@@ -1,14 +1,13 @@
 package com.example.hunterqrhunter;
 
 import android.os.Bundle;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NextScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.qr_screen);
+        setContentView(R.layout.login_screen);
     }
 }
--- app/src/main/java/com/example/hunterqrhunter/MainActivity.java
+++ app/src/main/java/com/example/hunterqrhunter/MainActivity.java
@@ -1,28 +1,15 @@
 package com.example.hunterqrhunter;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.hunterqrhunter.R;
-import com.example.hunterqrhunter.HashQR;
 import com.example.hunterqrhunter.data.FbRepository;
 import com.example.hunterqrhunter.model.QRCreature;
-import com.example.hunterqrhunter.model.User;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     Object hashVal = "yongbin@gmail.com";
@@ -56,14 +43,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button imageButton = (Button) findViewById(R.id.btn1);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        Button qrScreenBtn = (Button) findViewById(R.id.btn_qr_screen);
+        qrScreenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), NextScreen.class);
                 startActivity(intent);
             }
         });
+
+        Button userScreenBtn = (Button) findViewById(R.id.btn_user_screen) ;
+        userScreenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginScreen.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
