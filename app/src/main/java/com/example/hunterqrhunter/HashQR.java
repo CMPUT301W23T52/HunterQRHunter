8d128241ee635973c4f9eef894fce004cca1ecac
Everett Denman <denman@ualberta.ca>
2023-03-09 17:00:01 -0700

    Fixing hash & Adding scoreGen

--- app/src/main/java/com/example/hunterqrhunter/HashQR.java
+++ app/src/main/java/com/example/hunterqrhunter/HashQR.java
@@ -1,24 +1,30 @@
 package com.example.hunterqrhunter;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashQR {
     public int hash;
     public String HashName;
     //HashObject is a method that gets any type of object(QR code) returns hash code
-    public static int hashObject(Object obj) {
+    public static byte[] hashObject(String obj) {
         if (obj == null) {
-            return 0;
+            return null;
         }
-        int hash = obj.hashCode();
-        if (hash == Integer.MIN_VALUE) {
-            hash = 0;
-        } else if (hash < 0) {
-            hash = -hash;
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] data = obj.getBytes("UTF-8");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
         }
+
+        byte[] hash = messageDigest.digest(data);
         return hash;
     }
 
-    //NameGen() is a method that gets a hashed integer and generate a readable name out of that
-    public static String nameGen(int hash) {
+    //NameGen() is a method that gets a hash value and generates a readable name out of that
+    public static String nameGen(byte[] hash) {
         // Define an array of syllables that can be used to generate the random name
         String[] syllables = {"a", "an", "ba", "be", "ca", "ce", "da", "de", "el", "en", "fa", "fe", "ga", "ge", "ha", "he", "ia", "ie", "ja", "je", "ka", "ke", "la", "le", "ma", "me", "na", "ne", "oa", "oe", "pa", "pe", "ra", "re", "sa", "se", "ta", "te", "ua", "ue", "va", "ve", "wa", "we", "xa", "xe", "ya", "ye", "za", "ze"};
 
@@ -29,7 +35,7 @@ public class HashQR {
         StringBuilder builder = new StringBuilder();
 
         for (int i = 0; i < nameLength; i++) {
-            int index = (hash + i) % syllables.length;
+            int index = (Byte.toUnsignedInt(hash[i]) + i) % syllables.length;
             String syllable = syllables[index];
             // capitalize the first letter of the syllable to make it sound more like a name
             syllable = syllable.substring(0, 1).toUpperCase() + syllable.substring(1);
@@ -38,6 +44,28 @@ public class HashQR {
         String HashName = builder.toString();
 
         return HashName;
+    }
+
+    // scoreGen() is a method that takes in a hash value and generates an appropriate score
+    public static int scoreGen(byte[] hash) {
+        byte lastByte = hash[hash.length - 1];
+        int repeatCount = 0;
+        int score = 0;
+
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] == lastByte) {
+                repeatCount++;
+                if (i == hash.length - 1) {
+                    score += Byte.toUnsignedInt(lastByte)^repeatCount;
+                }
+            } else {
+                score += Byte.toUnsignedInt(lastByte)^repeatCount;
+                repeatCount = 0;
+            }
+            lastByte = hash[i];
+        }
+
+        return score;
     }
 }
 
