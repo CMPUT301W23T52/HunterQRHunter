873548bc760065d69e06c6d302c411bbb7c91a6b
Everett Denman <denman@ualberta.ca>
2023-03-12 22:36:16 -0600

    Something's come up, this is all i've got

--- app/src/main/java/com/example/hunterqrhunter/page/MyQRScreen.java
+++ app/src/main/java/com/example/hunterqrhunter/page/MyQRScreen.java
@@ -8,9 +8,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.hunterqrhunter.R;
 
 public class MyQRScreen extends AppCompatActivity {
+
+    private int pointSum;
+    private int totalQR;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_qr);
+
     }
+
+    // I don't know how to pull from the database here -Everett
+    public void findTotalQR() {
+        pointSum = 0;
+        for (int i = 0; i < db.length; i++) {
+            totalQR++;
+        }
+        // change the value of xml id "totalQR" to the value to totalQR
+    }
+
+    public void displayTotalPoints() {
+        pointSum = 0;
+        for (int i = 0; i < totalQR; i++) {
+            pointSum += dbCurrent;
+        }
+        // change the display value of xml id "totalScore" to the value of pointSum
+    }
+
 }
