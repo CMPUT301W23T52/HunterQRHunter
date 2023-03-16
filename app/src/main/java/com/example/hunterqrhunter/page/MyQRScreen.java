cb1b14279df5fe8f2deb0eaa597318893a817ec7
Everett Denman <denman@ualberta.ca>
2023-03-15 21:53:46 -0600

    Adding Firestore to MyQRScreen

--- app/src/main/java/com/example/hunterqrhunter/page/MyQRScreen.java
+++ app/src/main/java/com/example/hunterqrhunter/page/MyQRScreen.java
@@ -6,27 +6,33 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hunterqrhunter.R;
+import com.example.hunterqrhunter.data.FbRepository;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MyQRScreen extends AppCompatActivity {
 
     private int pointSum;
     private int totalQR;
 
+    private FirebaseFirestore db;
+
+    private FbRepository fb;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_qr);
 
+        db = FirebaseFirestore.getInstance();
+        fb = new FbRepository(db);
     }
 
-    // I don't know how to pull from the database here -Everett
     public void findTotalQR() {
         pointSum = 0;
         for (int i = 0; i < db.length; i++) {
             totalQR++;
         }
-        // change the value of xml id "totalQR" to the value to totalQR
+        // change the value of xml id "totalQR" to the value of totalQR
     }
 
     public void displayTotalPoints() {
