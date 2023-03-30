333fcf17b58378118a421292e6b4f78bba8b5a24
yongbinkim <kyongbin4@gmail.com>
2023-03-30 16:40:55 -0600

    My QR Screen

--- app/src/main/java/com/example/hunterqrhunter/page/MyQRScreen.java
+++ app/src/main/java/com/example/hunterqrhunter/page/MyQRScreen.java
@@ -1,12 +1,17 @@
 package com.example.hunterqrhunter.page;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hunterqrhunter.R;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MyQRScreen extends AppCompatActivity {
@@ -22,8 +27,22 @@ public class MyQRScreen extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_qr);
-        db = FirebaseFirestore.getInstance();
-        CollectionReference qrRef = db.collection("QR");
+        String userID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID); //get the device user ID
+        System.out.println(userID);
+        db = FirebaseFirestore.getInstance();//Initialize firestore database
 
+        CollectionReference qrRef = db.collection("QR");//get the QR collection
+        //if userID and qrRef's uid is the same, then get the score of the user
+        qrRef.whereEqualTo(FieldPath.of("uid", userID), userID).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+
+                for (DocumentSnapshot document : task.getResult()) {
+                    System.out.println(document.getData());
+                    Log.d("MyQRScreen", document.getId() + " => " + document.getData());
+                }
+            } else {
+                Log.d("MyQRScreen", "Error getting documents: ", task.getException());
+            }
+        });
     }
 }
