7df82bb07eb4b0b004a573e48eb3b1445e7b1005
dohyunkim <kdhminime@gmail.com>
2023-04-01 20:49:04 -0600

    design

--- app/src/main/java/com/example/hunterqrhunter/MainActivity.java
+++ app/src/main/java/com/example/hunterqrhunter/MainActivity.java
@@ -23,17 +23,7 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = findViewById(R.id.QrCreatureImage);
         // Generate hash and hash name
         byte[] hash = HashQR.hashObject(hashVal);
-        String HashName = HashQR.giveQrName(hash);
-//        Bitmap HashImage = hashQR.generateImageFromHashcode(hash);
 
-// Generate the bitmap from the hash code
-//        Bitmap bitmap = Bitmap.createBitmap(HashImage);
-
-// Set the bitmap on the ImageView
-//        imageView.setImageBitmap(bitmap);
-
-        // Initialize Firebase Firestore and UpdateCommend
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         // Initialize the button and set an OnClickListener
         Button mButton = findViewById(R.id.btn1);
@@ -44,9 +34,6 @@ public class MainActivity extends AppCompatActivity {
                 byte[] hash = HashQR.hashObject(hashVal);
                 String HashName = HashQR.giveQrName(hash);
 
-                // Create a new user with a first and last name, born year, hash, and hash name
-//                QRCreature qrCreature = new QRCreature(HashName, hash);
-//                fb.writeQR(qrCreature);
             }
         });
     }
